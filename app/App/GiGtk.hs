a2a3e473e51f80e0dfbc48856e940d7e9b2f3fcb
Jinnah Ali-Clarke <jinnah@noredink.com>
2026-07-22 17:37:01 -0400

    make reset button actually do something

--- app/App/GiGtk.hs
+++ app/App/GiGtk.hs
@@ -9,7 +9,8 @@ where
 import App.AppConfig (AppConfig (..))
 import qualified App.GiGtk.ColourBox as ColourBox
 import qualified App.GiGtk.Css as Css
-import Control.Monad (forM_, void)
+import Colour (Colour)
+import Control.Monad (forM, forM_, void)
 import qualified Data.Vector as Vector
 import qualified GI.Gio as Gio
 import qualified GI.Gtk as Gtk
@@ -38,19 +39,33 @@ activateGtkApp appConfig gtkApp = do
   mkColourBox <- ColourBox.initColourBoxConstructor display
 
   initialColours' <- initialColours appConfig
-  forM_ [0 .. Vector.length initialColours' - 1] $ \boxIdx -> do
-    colourBox <- mkColourBox boxIdx
-    let (colIdx, rowIdx) = fromIntegral boxIdx `divMod` numBoxesRows
-    Gtk.gridAttach grid (ColourBox.asWidget colourBox) colIdx rowIdx 1 1
-    ColourBox.setColour colourBox (initialColours' Vector.! boxIdx)
+  colourBoxes <- 
+    forM [0 .. Vector.length initialColours' - 1] $ \boxIdx -> do
+      colourBox <- mkColourBox boxIdx
+      let (colIdx, rowIdx) = fromIntegral boxIdx `divMod` numBoxesRows
+      Gtk.gridAttach grid (ColourBox.asWidget colourBox) colIdx rowIdx 1 1
+      pure colourBox
+  
+  updateCandidateColours colourBoxes initialColours'
 
   resetButton <- Gtk.buttonNewWithLabel "reset"
   Gtk.gridAttach grid resetButton 0 (numBoxesRows + 1) 1 1
+
+  void . Gtk.onButtonClicked resetButton $ do
+    resetSimulation appConfig
+    newInitialColours <- initialColours appConfig
+    updateCandidateColours colourBoxes newInitialColours
 
   rejectAllButton <- Gtk.buttonNewWithLabel "i don't like any of these"
   Gtk.gridAttach grid rejectAllButton 1 (numBoxesRows + 1) 2 1
 
   Gtk.windowPresent window
+
+updateCandidateColours :: [ColourBox.ColourBox] -> Vector.Vector Colour -> IO ()
+updateCandidateColours boxes colours = do
+  forM_ (zip boxes (Vector.toList colours)) $ \(box, colour) -> do
+    ColourBox.setColour box colour
+    ColourBox.setSelected box False
 
 app :: AppConfig -> IO ()
 app appConfig = do
